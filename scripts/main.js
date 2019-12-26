ae45de8c03f5e91c9829fbf5371a1f329c993393
Claudiapriegue <58549232+Claudiapriegue@users.noreply.github.com>
2019-12-26 14:07:08 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ var miImage = document.querySelector('img');
 miImage.onclick = function () {
     var miSrc = miImage.getAttribute('src');
     if (miSrc === 'images/firefox-icon.png') {
-      miImage.setAttribute('src','images/sunset2.png');
+      miImage.setAttribute('src','images/beach-party.png');
     } else {
       miImage.setAttribute('src', 'images/sunset-icon.png');
     }
