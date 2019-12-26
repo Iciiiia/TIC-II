7a7b5dab06561f8e1b45862068f8edb3e191f310
Claudiapriegue <58549232+Claudiapriegue@users.noreply.github.com>
2019-12-26 14:33:46 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,10 @@
 var miImage = document.querySelector('img');
 
 miImage.onclick = function () {
-    var miSrc = miImage.getAttribute("https://cdn.pixabay.com/photo/2018/03/03/08/08/sunset-3195123_960_720.jpg");
+    var miSrc = miImage.getAttribute('src');
     if (miSrc === 'images/firefox-icon.png') {
-      miImage.setAttribute("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Fiesta_provincial_del_Sol.jpg",'images/beach-party.png');
+      miImage.setAttribute('src','images/beach-party.png');
     } else {
-      miImage.setAttribute("https://cdn.pixabay.com/photo/2018/03/03/08/08/sunset-3195123_960_720.jpg", 'images/sunset-icon.png');
+      miImage.setAttribute('src', 'images/firefox-icon.png');
     }
-}
-var miBoton = document.querySelector('button');
-var miTitulo = document.querySelector( 'h1');
-
-function estableceNombreUsuario() {
-    var miNombre = prompt('Por favor, ingresa tu nombre.');
-    localStorage.setItem('nombre', miNombre);
-    miTitulo.textContent = 'Mozilla es fresco,' + miNombre;
-}
-
-if (!localStorage.getItem('nombre')) {
-    estableceNombreUsuario();
-}
-else {
-    var nombreAlmacenado = localStorage.getItem('nombre');
-    miTitulo.textContent = 'Mozilla es fresco,' + nombreAlmacenado;
-}
-
-miBoton.onclick = function() {
-    estableceNombreUsuario();
 }
