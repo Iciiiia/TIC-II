9f3cbb656c6e90577c8c51bd27210dec028a02d8
Claudiapriegue <58549232+Claudiapriegue@users.noreply.github.com>
2019-12-26 14:06:02 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,13 @@
+var miImage = document.querySelector('img');
+
+miImage.onclick = function () {
+    var miSrc = miImage.getAttribute('src');
+    if (miSrc === 'images/firefox-icon.png') {
+      miImage.setAttribute('src','images/sunset2.png');
+    } else {
+      miImage.setAttribute('src', 'images/sunset-icon.png');
+    }
+}
 var miBoton = document.querySelector('button');
 var miTitulo = document.querySelector( 'h1');
 
