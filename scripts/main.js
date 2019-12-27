0d385cb5d9d3c4e1e6c2af1c6446f5e8281bb2af
Iciiiia <58548668+Iciiiia@users.noreply.github.com>
2019-12-27 11:41:44 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,23 @@ var miImage = document.querySelector('img');
 miImage.onclick = function () {
     var miSrc = miImage.getAttribute('src');
     if (miSrc === 'images/sunset-icon2.jpg') {
-      miImage.setAttribute('src','images/sunset-1046475_640.jpg');
-    } else {
-      miImage.setAttribute('src', 'images/sunset-icon2.jpg');
-    }
+      miImage.setAttribute('src','images/sunset-1046475_640.jpg');}
+    else {miImage.setAttribute('src', 'images/sunset-icon2.jpg');}
 }
+
+var miBoton = document.querySelector('button');
+var miTitulo = document.querySelector( 'h1');
+function estableceNombreUsuario() {
+    var miNombre = prompt('Por favor, ingresa tu nombre.');
+    localStorage.setItem('nombre', miNombre);
+    miTitulo.textContent = 'Travel to Malibu,' + miNombre;
+}
+if (!localStorage.getItem('nombre')) {
+    estableceNombreUsuario();
+}
+else {
+    var nombreAlmacenado = localStorage.getItem('nombre');
+    miTitulo.textContent = 'Travel to Malibu,' + nombreAlmacenado;
+}
+miBoton.onclick = function() 
+{estableceNombreUsuario();}
